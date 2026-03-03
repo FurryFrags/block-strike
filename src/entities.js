c4a13dd9016f19587f589404a00a56ae5c208d0d
FurryFrags <furryfragz@gmail.com>
2026-03-03 18:16:31 +0700

    Rewrite shooter with full Three.js 3D world and character models

--- src/entities.js
+++ src/entities.js
@@ -1,3 +1,5 @@
+import * as THREE from 'https://unpkg.com/three@0.164.1/build/three.module.js';
+
 export class WeaponState {
   constructor(def) {
     this.def = def;
@@ -20,55 +22,72 @@ export class WeaponState {
     }
   }
 
-  canFire() {
-    return this.cooldown <= 0 && this.reloadLeft <= 0 && this.mag > 0;
-  }
-
   startReload() {
-    if (this.reloadLeft > 0 || this.mag === this.def.magSize || this.reserve <= 0) return false;
+    if (this.reloadLeft > 0 || this.mag >= this.def.magSize || this.reserve <= 0) return false;
     this.reloadLeft = this.def.reload;
     return true;
   }
 
   fire() {
-    if (!this.canFire()) return false;
+    if (this.cooldown > 0 || this.reloadLeft > 0 || this.mag <= 0) return false;
     this.mag -= 1;
     this.cooldown = 1 / this.def.fireRate;
     return true;
   }
 }
 
-export class Actor {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.radius = 0.22;
+function limb(size, color) {
+  const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(size.x, size.y, size.z),
+    new THREE.MeshStandardMaterial({ color, roughness: 0.7, metalness: 0.15 }),
+  );
+  mesh.castShadow = true;
+  mesh.receiveShadow = true;
+  return mesh;
+}
+
+export class BotCharacter {
+  constructor(x, z) {
+    this.group = new THREE.Group();
+    this.group.position.set(x, 0, z);
+    this.group.userData.kind = 'bot';
+
+    this.speed = 2.2;
     this.hp = 100;
-    this.dir = 0;
-    this.speed = 2.7;
+    this.attackCooldown = 0;
+    this.walkPhase = Math.random() * Math.PI * 2;
+
+    const torso = limb({ x: 0.54, y: 0.62, z: 0.28 }, '#4e708f');
+    torso.position.y = 1.42;
+    const head = limb({ x: 0.34, y: 0.34, z: 0.34 }, '#e7b99a');
+    head.position.y = 1.93;
+
+    this.leftArm = limb({ x: 0.14, y: 0.54, z: 0.16 }, '#35526d');
+    this.leftArm.position.set(-0.36, 1.42, 0);
+    this.rightArm = limb({ x: 0.14, y: 0.54, z: 0.16 }, '#35526d');
+    this.rightArm.position.set(0.36, 1.42, 0);
+
+    this.leftLeg = limb({ x: 0.17, y: 0.62, z: 0.2 }, '#28313a');
+    this.leftLeg.position.set(-0.14, 0.8, 0);
+    this.rightLeg = limb({ x: 0.17, y: 0.62, z: 0.2 }, '#28313a');
+    this.rightLeg.position.set(0.14, 0.8, 0);
+
+    this.gun = limb({ x: 0.1, y: 0.18, z: 0.54 }, '#212326');
+    this.gun.position.set(0.48, 1.33, 0.2);
+    this.gun.rotation.x = Math.PI * 0.5;
+
+    this.group.add(torso, head, this.leftArm, this.rightArm, this.leftLeg, this.rightLeg, this.gun);
   }
 
   get alive() {
     return this.hp > 0;
   }
-}
 
-export class Player extends Actor {
-  constructor(x, y, dir, weaponDef) {
-    super(x, y);
-    this.dir = dir;
-    this.pitch = 0;
-    this.weapon = new WeaponState(weaponDef);
-    this.score = 0;
-  }
-}
-
-export class Bot extends Actor {
-  constructor(x, y) {
-    super(x, y);
-    this.speed = 1.8;
-    this.dir = Math.random() * Math.PI * 2;
-    this.turnTimer = 0;
-    this.attackCooldown = 0;
+  animateWalk(time, speedFactor) {
+    const sway = Math.sin(time * 8 + this.walkPhase) * 0.55 * speedFactor;
+    this.leftLeg.rotation.x = sway;
+    this.rightLeg.rotation.x = -sway;
+    this.leftArm.rotation.x = -sway * 0.75;
+    this.rightArm.rotation.x = sway * 0.75;
   }
 }
